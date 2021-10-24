c7dc45876b69d8c0902496ca9166acacbdba4e8e
saurabhdohaiya <84789753+saurabhdohaiya@users.noreply.github.com>
2021-10-24 21:47:14 +0530

    Update Calculate.cpp

--- Calculate.cpp
+++ Calculate.cpp
@@ -32,7 +32,6 @@ int main() {
         default:
             // If the operator is other than +, -, * or /, error message is shown
             cout << "Error! operator is not correct";
-            break;
     }
 
     return 0;
